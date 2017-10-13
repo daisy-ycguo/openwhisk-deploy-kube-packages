9e374d99fd127515a30a176f1d245e96f4701bfc
daisy-ycguo <guoyingc@cn.ibm.com>
2017-10-13 07:03:09 +0000

    fix errors

--- docker/installCatalog.sh
+++ docker/installCatalog.sh
@@ -8,6 +8,7 @@ git clone https://github.com/apache/incubator-openwhisk-catalog.git
 pushd /incubator-openwhisk
   export OPENWHISK_HOME=$(pwd)
   curl https://openwhisk.ng.bluemix.net/cli/go/download/linux/amd64/wsk -o ./bin/wsk
+  chmod +x ./bin/wsk
 popd
 
 pushd /incubator-openwhisk-catalog
